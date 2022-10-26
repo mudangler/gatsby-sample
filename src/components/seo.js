dfeef4218d09008f478fb15ef17332d7d3539ace
t.iori <>
2022-10-26 18:22:33 +0900

    最初

---
+++ src/components/seo.js
@@ -0,0 +1,54 @@
+import React from "react"
+import Helmet from "react-helmet"
+import { graphql, useStaticQuery } from "gatsby"
+
+const Seo = (props) => {
+    const data = useStaticQuery(graphql`
+    query{
+        site{
+            siteMetadata{
+                title
+                lang
+                description
+                siteUrl
+                fbappid
+                locale
+            }
+        }
+    }`)
+
+    const title =  props.pageTitle ? props.pageTitle + " | " + data.site.siteMetadata.title : data.site.siteMetadata.title;
+
+    const description = props.pageDesc || data.site.siteMetadata.description;
+
+    const url = props.pagePath ? data.site.siteMetadata.siteUrl + props.pagePath : data.site.siteMetadata.siteUrl
+
+    const imgUrl = props.pageImg ? data.site.siteMetadata.siteUrl + props.pageImg :data.site.siteMetadata.siteUrl + `/thumb.jpg`
+
+    const imgW = props.pageImgW || 1280;
+    const imgH = props.pageImgH || 640;
+
+    return (
+        <Helmet>
+            <html lang={data.site.siteMetadata.lang} />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <link rel="canonical" href={url} />
+
+            <meta property="og:site_name" content={data.site.siteMetadata.title} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:url" content={url} />
+            <meta property="og:type" content="website" />
+            <meta property="og:locale" content={data.site.siteMetadata.locale} />
+            <meta property="fb:app_id" content={data.site.siteMetadata.fbappid} />
+            <meta property="og:image" content={imgUrl} />
+            <meta property="og:image:width" content={imgW} />
+            <meta property="og:image:height" content={imgH} />
+
+            <meta name="twitter:card" content="summary_large_image" />
+        </Helmet>
+    )
+}
+
+export default Seo
